78bdef493b857286bfb32e7c8fe7b7ff8b4c0e6f
alert <5718211151@qq.com>
2018-05-22 15:07:17 +0800

    initition

---
+++ README.md
@@ -0,0 +1,156 @@
+# Voice Command
+##### Extensible Chrome voice command tool
+
+
+### [View demo page](https://truthlabs.github.io/voice-command/)
+###### (Chrome only, requires microphone permissions).
+
+---
+
+Voice command uses Google's [Web Speech API](https://developers.google.com/web/updates/2013/01/Voice-Driven-Web-Apps-Introduction-to-the-Web-Speech-API) to turn speech into text, then uses an easily editable list of keywords to direct the search to the correct page.  Integrating new sites with keywords is simple and takes seconds, with basically no research and very little coding knowledge.
+
+I personally use a fork of this as my [new tab page](https://s.codepen.io/drewvosburg/debug/rOqdjj).
+
+## Keyword objects
+The code uses and array of keyword objects to search for what the user wants to do. Keyword objects are simple JSON objects with three properties: `keyword`, `url`, and an optional `suffix`.  The average keyword object looks like this: 
+```javascript
+{ keyword: 'Google', url: 'https://google.com/search?q=' }
+```
+
+The three components are fairly straightforward, but it's worth exploring in further detail.
+
+#### Keyword
+The `keyword` is either a string or an array of strings.  Arrays of strings can be in any order and may include spaces.
+
+#### URL
+The purpose of the `url` parameter is to provide a URL with the parameter name for a query for that specific page, which is often `?q=` at the end of a URL.  These can be easily reverse engineered from almost every website by performing a search on the target website and reducing the parameters in the resulting URL to only include the query.
+
+#### Suffix
+For some reason, some sites insist on having certain characters or parameters at the end of the URL to work properly.  For this reason, there is an optional `suffix` parameter.  If this is required, often it is a `/` or `&` character.
+
+#### Bonus: Command
+I only have used this once, but it's working around other behavior of the script.  If you say a keyword with no query string, like "Facebook," then it goes to the base URL of the URL found in that keyword object. For something like "Find my iPhone" where the base URL doesn't take you directly to the content, I had to create an exception.  This means that if you're going to a keyword with `"command":true`, it won't go to the base URL, but to the full URL.  Edge case, but it might be helpful.
+
+## Language parsing
+I wrote the string parsing myself.  Conceptually, I'm splitting the string up like so:
+
+![Prefix for human context, query string, precursor, keyword](./example.png)
+
+The two main pieces that I'm looking for are the query string (what you're searching for) and the keyword (where you're searching).
+
+#### Query string
+The brunt of the spoken phrase or sentence is likely to be the query string. This is the word or phrase that you want to find
+
+#### Worthless prefixes
+Imperative English sentences often begin with words that provide context to English speakers that the sentence is a command.  Usually this includes a verb.  I have created an array of common phrases that I have used to make commands that provide the script with no helpful information about context.  These are stripped out of the user's spoken sentence if they appear at the beginning.
+
+These are stored in an array of strings called `worthlessPrefixes`, which can easily be modified.
+
+#### Keyword
+The keyword helps me look up the corresponding keyword object to know what URL to send the query string to.
+
+#### Precursor
+The precursor is an optional piece that serves two functions. First, it helps users switch their sentences around syntactically, getting the same results for "Google Elon Musk" as "Elon Musk on Google." Secondly, it can serve as a tiebreaker in instances where there is more than one keyword.
+
+For example, say you wanted to see the Twitter accound for Google, but Google is a keyword. You can say "Google on Twitter," and the precursor "on" helps understand that we should be searching the next keyword, which is Twitter.  Without a precursor, I assume that the first keyword is the one we are searching, e.g. "Google Twitter."
+
+## Keyword Packs
+
+I built this to be extensible by "packs," which are JSON files with an object with a "keywords" array of keyword objects. The default included pack is based on the top 100 sites visited from the United States according to [Alexa rankings](http://www.alexa.com/topsites/countries/US). I excluded banks, pornographic sites, and marketing sites.
+
+The script loops through an array called `keywordPacks`, which is an array of strings that are URLs to the JSON files for each pack. On load, AJAX calls are made and the keywords from each pack are added to the `keywords` array.
+
+#### Supported services
+
+I made a default set of keywords to support the top 100 sites visited from the US according to Alexa rankings. I excluded banks, marketing sites, and pornographic sites, and included additional services that I find useful. The following is a list of the 89 services available in the default pack:
+
+- Google
+- Facebook
+- YouTube
+- Amazon
+- Find my iPhone
+- Yahoo
+- Wikipedia
+- eBay
+- Twitter
+- Reddit
+- Netflix
+- HBO Go
+- Soundcloud
+- JustWatch
+- Evernote
+- Hulu
+- Twitter
+- Gmail
+- Google Keep
+- Google Photos
+- Google Drive
+- Google Maps
+- Hulu
+- Dropbox
+- Speedtest.net
+- Airbnb
+- Instagram
+- Pandora
+- Spotify
+- The Noun Project
+- GitHub
+- Stackoverflow
+- Linkedin
+- Pinterest
+- Craigslist
+- DuckDuckGo
+- Imgur
+- Go.com
+- Bing
+- Tumblr
+- ESPN
+- CNN
+- Diply
+- New York Times
+- MSN
+- IMDb
+- Walmart
+- Apple
+- Yelp
+- OneDrive
+- Outlook
+- Etsy
+- Target
+- Aol
+- Buzzfeed
+- Home Depot
+- The Washington Post
+- Fox News
+- Indeed.com
+- TripAdvisor
+- Best Buy
+- Groupon
+- Gfycat
+- Slickdeals
+- Salesforce
+- Forbes
+- USA Today
+- Adobe
+- Lowe's
+- IRS.gov
+- Vice
+- CNet
+- Twtich.tv
+- Macy's
+- Ask.com
+- About.com
+- MLB.com
+- National Institutes of Health
+- CBS Sports
+- Business Insider
+- Kohl's
+- Dictionary.com
+- Weather.com
+- Blogspot
+- Huffington Post
+- Wordpress
+- Realtor.com
+- Zillow
+- Wikia
+- The Daily Mail
---
+++ voice-command.js
@@ -0,0 +1,469 @@
+// ----- On render -----
+$(function() {
+  
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  
+  if (!isChrome) {
+    unsupported();
+  }
+
+  setTimeout(function(){
+    $('#newPage').removeClass('loading')
+  }, 10);
+  
+  var tip;
+  
+  var defaultBehavior = {
+    url: 'https://www.google.com/search?q=',
+    firstHit: 'https://www.google.com/search?btnI&q='
+  };
+  // You can easily switch from Google to other default search engines here
+  /*defaultBehavior = {
+    url: 'https://duckduckgo.com/?q=',
+    firstHit: 'https://duckduckgo.com/?q=!'
+  }*/
+  var keywords = [{
+    "keyword":[
+      "weather be like",
+      "weather going to be like",
+      "weather going to be",
+      "weather going to be",
+      "weather like",
+      "weather be",
+      "weather"
+    ],
+    "url": "defaultBehavior.url + weather%20"
+  }, {
+    "keyword": ["Go to", "goto", "open"],
+    "url": "defaultBehavior.firstHit"
+  }, {
+    "keyword":"my iphone",
+    "url":"https:\/\/www.icloud.com\/#find",
+    "command":true
+  }];
+
+  // You can add keywords in bulk by JSON file here.
+  var keywordPacks = [
+    "./keywords.json"
+  ]
+
+  var precursors = [
+    'on',
+    'on my',
+    'in',
+    'in my',
+    'from',
+    'from my',
+    "'",
+    "'s",
+    "at",
+    "sell",
+    "have"
+  ];
+  var worthlessPrefixes = [
+    "what's the",
+    "what is the",
+    "what will the",
+    "find",
+    "show me",
+    "show",
+    "i want to",
+    "i want",
+    "give me",
+    "help me find",
+    "help me",
+    "let me",
+    "search for",
+    "look up",
+    "look for",
+    "search for",
+    "search",
+    "i need",
+    "i need to",
+    "i need to get",
+    "i need to have",
+    "play",
+    "is",
+    "are",
+    "does",
+    "do they sell",
+    "do they have",
+    "can I buy",
+    "can I get"
+  ];
+  var final_transcript = '';
+  var recognizing = false;
+  var cancel = false;
+  
+  worthlessPrefixes.sort(lengthSort).reverse();
+  precursors.sort(lengthSort).reverse();
+
+
+  // Grab any keyword packs
+  $.each(keywordPacks, function(){
+    $.getJSON(this, function(data){
+      if (data.keywords) {
+        keywords = data.keywords.concat(keywords);
+      }
+    })
+  })
+
+  // Check to see if webkitSpeechRecognition is available
+  if (!('webkitSpeechRecognition' in window)) {
+    unsupported();
+  } else {
+    var recognition = new webkitSpeechRecognition();
+    recognition.interimResults = true;
+    recognition.continuous = true;
+    recognition.onstart = function() {}
+    recognition.onresult = function(e) {
+      var interim_transcript = '';
+
+      for (var i = e.resultIndex; i < e.results.length; ++i) {
+        if (e.results[i].isFinal) {
+          final_transcript = final_transcript + e.results[i][0].transcript;
+        } else {
+          interim_transcript = interim_transcript + e.results[i][0].transcript;
+        }
+      }
+      final_transcript = capitalize(final_transcript);
+      $('#final_span').empty().html(linebreak(final_transcript));
+      $('#interim_span').empty().html(linebreak(interim_transcript));
+    };
+  }
+  recognition.onerror = function(e) {}
+
+  function startButton(event) {
+    final_transcript = '';
+    //recognition.lang = select_dialect.value;
+    recognition.start();
+  }
+
+  function unsupported() {
+    console.log('Webkit speech api not supported in your browser');
+    $('#unsupported').addClass('unsupported');
+  }
+
+  // Main start event
+  $('#button').on('mousedown touchstart', function() {
+    if (cancel) {
+      TweenMax.killAll();
+      reset();
+    } else {
+      startRecog();
+    }
+  });
+
+  function startRecog() {
+    if (!recognizing) {
+      recognition.start();
+      final_transcript = '';
+      $('#final_span').empty();
+      $('#interim_span').empty();
+      $(this).text('done');
+      recognizing = true;
+    }
+  };
+
+  $(document).on('touchend mouseup', function() {
+    endRecog();
+  })
+
+  function endRecog() {
+    if (recognizing) {
+      recognizing = false;
+      recognition.stop();
+    }
+  }
+
+  recognition.onend = function() {
+    var string = final_transcript.toLowerCase(); 
+    if (string.trim() != '') {
+      recognizing = false;
+      cancel = true;
+      var counter = {
+          t: 0
+        },
+        border = '2px solid white',
+        loading = $('<div class="element"><div class="loading"></div><div class="slice"></div></div>'),
+        fill = $('<div class="loading ring">')
+      $('#button #contents').append(loading).append(fill);
+      $('#button').addClass('cancel');
+      TweenMax.to(counter, 2, {
+        t: 100,
+        ease: Linear.easeNone,
+        onUpdate: function() {
+          TweenMax.set($('#button .element .loading'), {
+            rotation: (counter.t * 3.6) - 45
+          });
+          if (counter.t >= 25) {
+            $('#button > #contents > .loading.ring').css('border-top', border);
+          };
+          if (counter.t >= 50) {
+            $('#button > #contents > .loading.ring').css('border-right', border);
+            $('#button .element .slice').remove();
+          }
+          if (counter.t >= 75) {
+            $('#button > #contents > .loading.ring').css('border-bottom', border);
+          }
+        },
+        onComplete: function() {
+          process(string);
+          $('#newPage').addClass('going');
+        }
+      });
+    } else {
+      showTip();
+    }
+  }
+
+  function process(string) {
+    string = string.toLowerCase();
+    success = false;
+    var foundKeywords = [];
+    $.each(keywords, function() {
+      if ($.isArray(this.keyword)) {
+        var self = this;
+        this.keyword.sort(lengthSort).reverse();
+        $(this.keyword).each(function() {
+          var keyword = this.toLowerCase(),
+              keywordIndex = String(string).indexOf(keyword);
+          if (keywordIndex > -1) {
+            foundKeywords.push({
+              'keywordIndex': keywordIndex,
+              'keywordObject': self,
+              'keyword': keyword
+            });
+          }
+        });
+      } else {
+        var keyword = this.keyword.toLowerCase(),
+          keywordIndex = String(string).indexOf(keyword);
+        if (keywordIndex > -1) {
+          foundKeywords.push({
+            'keywordIndex': keywordIndex,
+            'keywordObject': this,
+            'keyword': this.keyword
+          });
+        }
+      }
+    });
+    if (!foundKeywords.length) {
+      noKeyword(string);
+    } else if (foundKeywords.length == 1) {
+      // String ops
+      var passIt = foundKeywords[0].keywordObject;
+      passIt.foundKeyword = foundKeywords[0].keyword;
+      parseString(string, passIt);
+    } else {
+      // At this point, we know more than one keyword is in here.
+      // Now we need to determine which is the command
+      // and which is part of a query.
+      var youngestKeyword,
+          youngestIndex = 10000000000000000000000000000,
+          finished = false;
+      $.each(foundKeywords, function() {
+        if (youngestIndex > this.keywordIndex) {
+          youngestKeyword = this.keywordObject;
+          youngestKeyword.foundKeyword = this.keyword;
+          youngestIndex = this.keywordIndex;
+        }
+        var currentKeywordObject = this.keywordObject;
+        var currentKeyword = this.keyword
+        $.each(precursors, function(){
+          var thisString = String(string).toLowerCase();
+          var precursorIndex = thisString.indexOf(String(this).toLowerCase()),
+              proposedIndex = precursorIndex + String(this).length,
+              actualIndex = thisString.indexOf(String(currentKeyword).toLowerCase());
+          if (precursorIndex > 0){
+            if (proposedIndex == actualIndex || proposedIndex + 1 == actualIndex) {
+              currentKeywordObject.foundKeyword = currentKeyword;
+              parseString(string, currentKeywordObject);
+              finished = true;
+              return false;
+            }
+          }
+        })
+        if (finished) {
+          return false;
+        }
+      })
+      if (!finished) {
+        parseString(string, youngestKeyword);
+      }
+    }
+  }
+
+  function noKeyword(string) {
+    $(worthlessPrefixes).each(function() {
+      if (string.indexOf(this.toLowerCase()) == 0) {
+        string = string.substring(this.length + 1);
+        return false;
+      }
+    });
+    getThat(defaultBehavior, string.trim());
+  }
+
+  function parseString(string, keyword) {
+    var found = false;
+    // strip out useless human context
+    $(worthlessPrefixes).each(function() {
+      if (string.indexOf(this) == 0) {
+        string = string.substring(this.length + 1);
+        return false;
+      }
+    });
+    if (string.trim() == String(keyword.foundKeyword).toLowerCase()) {
+        // There is no string here, so go to the root domain
+      if (!keyword.command) {
+        var pathArray = keyword.url.split('/'),
+          protocol = pathArray[0],
+          host = pathArray[2],
+          url = protocol + '//' + host;
+        newKeyword = {
+          url: url
+        };
+      }
+      else {
+        newKeyword = {
+          url: keyword.url
+        }
+      }
+      getThat(newKeyword, '');
+    } else {
+      // There is a string here, so query it
+      $(precursors).each(function() {
+        var onKeyword = String(this) + ' ' + keyword.foundKeyword.toLowerCase();
+        if (string.indexOf(onKeyword) > -1 && string.indexOf(onKeyword) == string.length - onKeyword.length) {
+          string = string.substring(0, string.length - onKeyword.length).trim();
+          found = true;
+          return false
+        }
+      });
+
+      var searchXFor = keyword.foundKeyword.toLowerCase() + ' for';
+      if (string.indexOf(searchXFor) == 0 && !found) {
+        string = string.substring(searchXFor.length + 1);
+      }
+
+      if (!found) {
+        string = string.replace(keyword.foundKeyword.toLowerCase(), '')
+      }
+      getThat(keyword, string.trim());
+    }
+  }
+
+  function getThat(command, query, firstHit) {
+    var suffix = '';
+    if (command.suffix) {
+      suffix = command.suffix
+    }
+    if (firstHit) {
+      openIt(defaultBehavior.firstHit + encodeURIComponent(query) + suffix);
+    } else {
+      command.url = command.url.replace("defaultBehavior.url + ", defaultBehavior.url)
+        .replace("defaultBehavior.firstHit + ", defaultBehavior.firstHit)
+        .replace("defaultBehavior.url", defaultBehavior.url)
+        .replace("defaultBehavior.firstHit", defaultBehavior.firstHit);
+      openIt(command.url + encodeURIComponent(query) + suffix);
+    }
+  }
+
+  function openIt(url) {
+    window.location.href = url;
+  }
+
+  function reset() {
+    TweenMax.set($("#button .loading"), {
+      clearProps: "all"
+    });
+    $('#button').removeClass('cancel');
+    $('#button #contents').empty();
+    cancel = false;
+    final_transcript = '';
+    $('#final_span').text('');
+  }
+
+  function showTip() {
+    reset();
+    $('#tip').addClass('show');
+    if (tip) {
+      window.clearTimeout(tip);
+    }
+    tip = setTimeout(function() {
+      $('#tip').removeClass('show');
+    }, 3000);
+
+  }
+
+  /// ##### VISUALIZATION STUFF #####
+
+  var liveSource;
+  var analyser;
+  var frequencyData;
+  var scaling = 1.5;
+
+  function update() {
+      requestAnimationFrame(update);
+
+      if (recognizing) {
+        analyser.getByteFrequencyData(frequencyData);
+        TweenMax.set($('.visual'), {
+          autoAlpha: 0.75
+        });
+
+        TweenMax.set($('#viz1'), {
+          scale: (((frequencyData[8] + 1) / 100) / scaling)
+        });
+        TweenMax.set($('#viz2'), {
+          scale: (((frequencyData[15] + 1) / 100) / scaling)
+        });
+        TweenMax.set($('#viz3'), {
+          scale: (((frequencyData[21] + 1) / 100) / scaling)
+        });
+      } else {
+        TweenMax.set($('.visual'), {
+          autoAlpha: 0
+        })
+      }
+    }
+    // creates an audiocontext and hooks up the audio input
+  var context = new AudioContext();
+  navigator.webkitGetUserMedia({
+    audio: true
+  }, function(stream) {
+    console.log("Connected live audio input");
+    if (!analyser) {
+      liveSource = context.createMediaStreamSource(stream);
+      // Create the analyser
+      analyser = context.createAnalyser();
+      analyser.smoothingTimeConstant = 0.3;
+      analyser.fftSize = 64;
+      frequencyData = new Uint8Array(analyser.frequencyBinCount);
+      liveSource.connect(analyser);
+    };
+    update();
+  }, function() {
+    console.log('Error connecting to audio')
+  });
+
+});
+
+/// ##### BASIC UTILS #####
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function linebreak(s) {
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+function lengthSort(astr, bstr) {
+ if (astr.length != bstr.length) {
+  return astr.length - bstr.length;
+ }
+ return (astr < bstr) ? -1 : (astr > bstr) ? 1 : 0;
+};
